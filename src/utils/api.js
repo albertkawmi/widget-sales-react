f93522ff09558ee952889867d05b0def38ea940d
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-14 21:01:07 +0000

    Create basic API helper util

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+const BASE_API_URL = 'http://localhost:4000'
+
+const fetchJson = path =>
+  fetch([BASE_API_URL, path].join(''))
+    .then(r => r.json())
+
+const endpoints = {
+  get: {
+    clients: () => fetchJson('/clients'),
+    sales: () => fetchJson('/sales'),
+    fault: () => fetchJson('/fault'),
+  }
+}
+
+export default {
+  get: endpoint => {
+    if (!(endpoint in endpoints.get)) {
+      return Promise.reject('api.get called with invalid endpoint')
+    }
+    return endpoints.get[endpoint]()
+  }
+  // other API methods would go here
+}
