193198df3b5d18eded7020012cf8b9b1bcc6f836
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-14 21:59:41 +0000

    Add Fault route handler

--- src/routes/Fault/Fault.route.js
+++ src/routes/Fault/Fault.route.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import Store from '../../components/Store'
 
-export const Fault = () => (
-  <h2>Faulty Page</h2>
-)
+// This component hits an endpoint that always returns 500
+// in order to demonstrate basic error handling
+
+export class Fault extends React.Component {
+  componentWillMount() {
+    this.context.fetchFault()
+  }
+  render() {
+    return (
+      <div>
+        <h2>A Faulty Page</h2>
+        <p>When something goes wrong you'll see an error message.</p>
+      </div>
+    )
+  }
+}
+
+Fault.contextTypes = Store.childContextTypes
