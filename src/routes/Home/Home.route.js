ed1d9d185f1e58364a824ca11cf392e61c1614b3
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-15 00:55:30 +0000

    Adjust styles for the home route

--- src/routes/Home/Home.route.js
+++ src/routes/Home/Home.route.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Home = ({ clients = 0, sales = 0 }) => (
-  <div>
+export const Home = () => (
+  <div className="home">
     <h2>Welcome</h2>
     <p>What would you like to do?</p>
     <Link to="/clients">
-      <button>View all your Clients</button>
+      <button className="mr">View all your Clients</button>
     </Link>
     <Link to="/sales">
-      <button>View all your Sales</button>
+      <button className="mr">View all your Sales</button>
     </Link>
     <Link to="/fault">
       <button>Actually, I'd like to see an error page instead</button>
