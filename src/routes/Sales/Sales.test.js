bfa032294f924005106368da695f53f97905f71b
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-16 00:44:07 +0000

    Improve tests for Sales route

--- src/routes/Sales/Sales.test.js
+++ src/routes/Sales/Sales.test.js
@@ -1,24 +1,49 @@
 /* eslint-env jest */
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 import Sales from './Sales.route'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const salesRoute = Enzyme.shallow(
-  <Router>
-    <Sales />
-  </Router>
-)
-
 describe('<Sales />', () => {
-  it('renders without crashing', () => {
-    expect(salesRoute).toBeTruthy()
+  it('renders a sales table', () => {
+    const context = {
+      sales: [],
+      fetchSales: jest.fn()
+    }
+    const wrapper = Enzyme.shallow(
+      <Sales />,
+      { context }
+    )
+    expect(
+      wrapper.find('SalesTable')
+    ).toBeTruthy()
+
+    expect(wrapper).toMatchSnapshot()
   })
 
-  it('has not been changed unintentionally', () => {
-    expect(salesRoute).toMatchSnapshot()
+  it('fetches data if sales is empty', () => {
+    const context = {
+      sales: [],
+      fetchSales: jest.fn()
+    }
+    Enzyme.shallow(
+      <Sales />,
+      { context }
+    )
+    expect(context.fetchSales).toHaveBeenCalled()
+  })
+
+  it('does not fetch if there are existing sales', () => {
+    const context = {
+      sales: [{ id: 1}],
+      fetchSales: jest.fn()
+    }
+    Enzyme.shallow(
+      <Sales />,
+      { context }
+    )
+    expect(context.fetchSales).not.toHaveBeenCalled()
   })
 })
