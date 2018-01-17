e296825609ef5c34b66d711c27e4c37290c30287
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-17 21:27:28 +0000

    Remove logo image

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../images/logo.png'
 import Nav from '../Nav'
 import './Header.css'
 
 const Header = () => (
   <div className="header-bg">
     <header className="wrap">
-      <img className="logo" src={logo} alt="WidgetSales Logo"/>
       <Link className="site-title-link" to="/">
         <h1 className="site-title">
           Widget<span className="site-title__highlight">Sales</span>
