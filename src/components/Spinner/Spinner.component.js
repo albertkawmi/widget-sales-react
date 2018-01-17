be3c110f4a3c03afda5c1d4d0e6b1dda6a7998ab
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-17 21:04:04 +0000

    Add Spinner component

---
+++ src/components/Spinner/Spinner.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../images/logo.png'
+import './Spinner.css'
+
+const Spinner = () => (
+  <div className="spinner-outer">
+    <img className="spinner" src={logo} alt="Loading..."/>
+    <a className="spinner-link" href="/why-so-slow">Why is this taking so long?</a>
+  </div>
+)
+
+export default Spinner
