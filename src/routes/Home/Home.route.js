58736a3ea4f04992489fc42b4a8e8d6f5ba7ce24
Albert Kawmi <albert.kawmi@gmail.com>
2018-01-19 00:40:12 +0000

    Make homepage layout a bit cleaner

--- src/routes/Home/Home.route.js
+++ src/routes/Home/Home.route.js
@@ -3,23 +3,25 @@ import LinkButton from '../../components/LinkButton'
 
 const Home = () => (
   <div className="home">
-    <h2 className="page-heading">Welcome</h2>
-    <p>What would you like to do?</p>
-    <LinkButton
-      to="/clients"
-      text="View all your Clients"
-      buttonClassName="mr mt"
-    />
-    <LinkButton
-      to="/sales"
-      text="View all your Sales"
-      buttonClassName="mr mt"
-    />
-    <LinkButton
-      to="/fault"
-      text="Actually, I'd like to see an error page instead"
-      buttonClassName="mr mt"
-    />
+    <section className="homepage-box">
+      <h2 className="page-heading page-heading--homepage">Welcome</h2>
+      <p>What would you like to do?</p>
+      <LinkButton
+        to="/clients"
+        text="View all your Clients"
+        buttonClassName="homepage-btn"
+      />
+      <LinkButton
+        to="/sales"
+        text="View all your Sales"
+        buttonClassName="homepage-btn"
+      />
+      <LinkButton
+        to="/fault"
+        text="Actually, I'd like to see an error page instead"
+        buttonClassName="homepage-btn"
+      />
+    </section>
   </div>
 )
 
